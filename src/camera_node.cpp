0ea313b805ef761804d23e90444a179a3b7289fe
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-10-19 03:17:26 -0600

    included camera node

---
+++ src/camera_node.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("Initializing Argus Sensors...\n");
+
+  // Argus Sensor setup or something idk
+
+  printf("Argus Sensors Online.\n");
+  return 0;
+}
